ef2e2700e3d073fa57099d22682b03e43604ca37
aminelhm <131161302+aminelhm@users.noreply.github.com>
2025-04-23 23:12:38 +0200

    definition de models

--- app/models/question_model.py
+++ app/models/question_model.py
@@ -1,9 +1,7 @@
 from pydantic import BaseModel
 
-# Requête reçue : l’utilisateur pose une question
 class QuestionRequest(BaseModel):
     question: str
 
-# Réponse renvoyée : l’IA répond à la question
 class AnswerResponse(BaseModel):
     answer: str
