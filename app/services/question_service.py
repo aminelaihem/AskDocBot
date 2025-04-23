6b2ef4bc61e6fc8acb24a192fd63d4a05e766d40
aminelhm <131161302+aminelhm@users.noreply.github.com>
2025-04-23 23:36:35 +0200

    warnings fix

--- app/services/question_service.py
+++ app/services/question_service.py
@@ -1,21 +1,20 @@
-import os
-from langchain.llms import OpenAI
+from langchain_openai import ChatOpenAI 
+from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
-
-load_dotenv()
-
-llm = OpenAI(
-    temperature=0.7,
-    model_name="gpt-3.5-turbo",
-    openai_api_key=os.getenv("OPENAI_API_KEY")
-)
-
-template = PromptTemplate(
-    input_variables=["question"],
-    template="Explique-moi simplement : {question}"
-)
 
 def get_answer(question: str) -> str:
-    prompt = template.format(question=question)
-    return llm(prompt)
+
+    llm = ChatOpenAI(
+        temperature=0.7,  
+        model_name="gpt-3.5-turbo"  
+    )
+
+    prompt = PromptTemplate(
+        input_variables=["question"],
+        template="Réponds clairement à cette question : {question}"
+    )
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    response = chain.run({"question": question})
+    return response
